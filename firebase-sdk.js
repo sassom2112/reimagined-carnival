8f99aca5b1ecfcf663d3554b1c9765ab12acae19
Mic <sassom2112@gmail.com>
2024-06-16 17:47:39 -1000

    Save current progress before removing sensitive information

--- firebase-sdk.js
+++ firebase-sdk.js
@@ -3,14 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVS46HI-6_5n9BSm9dHfhFJSbSfXLJ0D4",
-  authDomain: "myfirstfirebasepro-f1a95.firebaseapp.com",
-  projectId: "myfirstfirebasepro-f1a95",
-  storageBucket: "myfirstfirebasepro-f1a95.appspot.com",
-  messagingSenderId: "711987225553",
-  appId: "1:711987225553:web:904ba81c82a32293feb6f0"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export default firebaseConfig;
